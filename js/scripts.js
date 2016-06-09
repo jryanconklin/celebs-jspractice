571dc230e39f102519b3d6ce8e88409e7fd1746f
J. Ryan Conklin <jryanconklin@gmail.com>
2016-06-08 18:16:27 -0700

    Fix the Form Show/Hide Bug for Celebs

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $("form#celebs").submit(function(event) {
     var gender = $("select#gender").val();
     var dog = $("select#dog").val();
-    var age = parseInt($("select#age").val());
+    var age = parseInt($("#age").val());
     var food = $("select#food").val();
 
     //if (gender === 'female' && age < 35) {
@@ -17,14 +17,18 @@ $(document).ready(function() {
 
     if (gender === "female" && age < 35) {
       $('#gosling').show();
+      $('#clooney').hide();
     } else if (age > 35){
       $('#clooney').show();
+      $('#gosling').hide();
     }
 
     if (gender === 'male' && age < 45) {
       $('#swift').show();
+      $('#bullock').hide();
     } else if (gender === 'male' && age > 45){
       $('#bullock').show();
+      $('#swift').hide();
     }
 
     event.preventDefault();
